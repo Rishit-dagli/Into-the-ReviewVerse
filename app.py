83d8ec709f84f6bed3249b7578d234f622ea3c7a
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-07-15 16:48:20 +0000

    Fixing flask

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template
-
-app = Flask(__name__, template_folder = 'templates',static_folder = 'css')
-
+# how to use css in python_ flask
+# flask render_template example
+ 
+from flask import Flask, render_template
+ 
+# WSGI Application
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name
+app = Flask(__name__, template_folder='templates', static_folder='static')
+ 
+# @app.route('/')
+# def welcome():
+#     return "This is the home page of Flask Application"
+ 
 @app.route('/')
 def index():
     return render_template('index.html')
-if __name__ == '__main__':
-    app.run(debug=True)
+ 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+if __name__=='__main__':
+    app.run(debug = True)
