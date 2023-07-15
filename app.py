dea2a114271586386a749697b578a1d8334bc113
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-07-15 17:31:19 +0000

    Fixed flask

--- app.py
+++ app.py
@@ -16,9 +16,13 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 def index():
     return render_template('index.html')
  
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/task/')
+def task():
+    return render_template('task.html')
 
 if __name__=='__main__':
     app.run(debug = True)
