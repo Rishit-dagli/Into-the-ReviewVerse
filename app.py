0f961ae2fa7d1c02c9bccecbd438e60f0bd84fdf
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-07-16 01:56:29 +0000

    ML changes

--- app.py
+++ app.py
@@ -14,14 +14,71 @@ def search():
         title, store, review_nums, reviews = scrap(url)
         infer = []
         for val in ['pros', 'cons']:
+            out = inference(reviews, val)
+            infer.append(out)
+        pros = infer[0]
+        n = len(pros)
+        if n >= 1:
+            pro1 = pros[0]
+        else:
+            pro1 = ''
+        if n >= 2:
+            pro2 = pros[1]
+        else:
+            pro2 = ''
+        if n >= 3:
+            pro3 = pros[2]
+        else:
+            pro3 = ''
+        if n >= 4:
+            pro4 = pros[3]
+        else:
+            pro4 = ''
+        if n >= 5:
+            pro5 = pros[4]
+        else:
+            pro5 = ''
+        if n >= 6:
+            pro6 = pros[5]
+        else:
+            pro6 = ''
+
+        cons = infer[1]
+        n = len(cons)
+        if n >= 1:
+            con1 = cons[0]
+        else:
+            con1 = ''
+        if n >= 2:
+            con2 = cons[1]
+        else:
+            con2 = ''
+        if n >= 3:
+            con3 = cons[2]
+        else:
+            con3 = ''
+        if n >= 4:
+            con4 = cons[3]
+        else:
+            con4 = ''
+        if n >= 5:
+            con5 = cons[4]
+        else:
+            con5 = ''
+        if n >= 6:
+            con6 = cons[5]
+        else:
+            con6 = ''
+
+
             infer.append(inference(reviews, val))
-        return render_template('task.html', product=title+' - ', store = store, pros=infer[0], cons=infer[1], review_nums=str(review_nums))
+        return render_template('task.html', product=title+' - ', store = store, review_nums=str(review_nums), pro1=pro1, pro2=pro2, pro3=pro3, pro4=pro4, pro5=pro5, pro6=pro6, con1=con1, con2=con2, con3=con3, con4=con4, con5=con5, con6=con6)
     except: 
         title = 'try again'
         store = 'try agian'
         review_nums = 'try again'
         reviews = ['try again', 'try again', 'try again', 'try again']
-        return render_template('task.html', product=title, store = store, pros=reviews[0], cons=reviews[2], review_nums=str(review_nums))
+        return render_template('task.html', product=title, store = store, review_nums=str(review_nums), pro1='try again', pro2='try again', pro3='try again', pro4='try again', pro5='try again', pro6='try again', con1='try again', con2='try again', con3='try again', con4='try again', con5='try again', con6='try again')
 
 @app.route('/')
 def index():
--- model/inference.py
+++ model/inference.py
@@ -13,11 +13,8 @@ def inference(reviews, val):
     out = out[0]['generated_text'].split('\n')
     out.pop(0)
     for i in range(len(out)):
-        out[i] = out[i][2:]
-    points = ''
-    for j in out:
-        points += j + '.' + ' '
+        out[i] = out[i][2:] + '.'
 
-    return points
+    return out
 
 # print(inference('<|prompter|>Given the reviews of this product give me its cons in a list format only. This turned out amazing! So cute and such good quality. The shop owner even took the liberty of putting photos together with the same color scheme/tones for a more cohesive layout. Just perfect! The book arrived very quickly, considering it’s all custom made! It is very small, which maybe I just didn’t notice in the photos. But I was mostly bummed by the cropping/arrangement of the book. It would have been really nice if a preview was sent first? I was hoping all my photos would be placed vertically and cropped appropriately, but it was organized and laid out according to what the seller thought I wanted without any consultation. And some had to have the blurred edges just to fit in that orientation. It’s definitely a cool concept, but it just didn’t compare to the photos I saw in the post The photos are so high quality. I’m not sure what I was expecting when I ordered but it went above my expectations. Shipping is also fast and I can’t wait to give the little book as a present. I will be ordering from them again The magnet that holds the book closed is really not great quality. Other than that I am very pleased and I absolutely love my keychain!<|endoftext|><|assistant|>'))
